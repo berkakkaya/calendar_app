661963195239b8f79882546337b45a2b6c18e3bd
Berk Akkaya <berkakkaya000@gmail.com>
2023-05-24 18:18:20 +0300

    add: wrong email or password string

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,7 +1,9 @@
-const String welcomeScreenText =
+const welcomeScreenText =
     "Farklı türden etkinlikler oluşturun, farklı katılımcılar ekleyin ve bildirimler sayesinde etkinlikleri asla kaçırmayın.";
 
-const String apiUrlWarning =
+const apiUrlWarning =
     "API adresiniz düzgün ayarlanmamış. Lütfen kontrol edip yeniden deneyin.";
 
 const apiTestString = "Hello world!";
+const wrongEmailOrPassword =
+    "Yanlış e-posta veya şifre girdiniz. Lütfen girişlerinizi kontrol edip yeniden deneyiniz.";
