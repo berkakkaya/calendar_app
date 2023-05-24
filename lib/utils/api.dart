9ae36bc0e47ffa545ea26777ecf501e37e9aa86e
Berk Akkaya <berkakkaya000@gmail.com>
2023-05-24 17:53:16 +0300

    docs: write docs for API manager

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -8,6 +8,7 @@ class ApiManager {
   static String? _apiUrl;
   static bool isReady = false;
 
+  // Setup Dio
   static final Dio _dio = Dio(BaseOptions(
     sendTimeout: const Duration(seconds: 20),
     connectTimeout: const Duration(seconds: 20),
@@ -18,6 +19,11 @@ class ApiManager {
     responseType: ResponseType.json,
   ));
 
+  /// Initializes the API manager with given base URL
+  ///
+  /// This function checks if the given base URL is working and finalizes the
+  /// initialization process. Other functions of the API manager cannot be used
+  /// without a proper initialization process were made.
   static Future<void> initialize({required String baseUrl}) async {
     _apiUrl = Uri.tryParse(baseUrl).toString();
 
@@ -37,6 +43,14 @@ class ApiManager {
     }
   }
 
+  /// Log into an account and returns tokens
+  ///
+  /// This function logs in to an account with given email and password.
+  /// If successful, `Authentication` object will include an access token
+  /// and a refresh token.
+  /// If the API manager isn't ready, the response status will be `none`.
+  /// If the email or the password is wrong, response status will be
+  /// `wrongEmailOrPassword`.
   static Future<Authentication> login({
     required String email,
     required String password,
@@ -59,6 +73,12 @@ class ApiManager {
     return Authentication.fromJson(ResponseStatus.success, response.data);
   }
 
+  /// Create a new account and log into it
+  ///
+  /// This function creates a new account based on the given info and tries to
+  /// create a new account. If successfull, `Authentication` object will include
+  /// an access token and a refresh token. If an account with similar username
+  /// and/or similar email exists, status will be set to `duplicateExists`.
   static Future<Authentication> register({
     required String name,
     required String surname,
