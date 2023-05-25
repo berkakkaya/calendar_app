85e14d1325a47bd98ed62ad68955b3f5ef6c47c7
Berk Akkaya <berkakkaya000@gmail.com>
2023-05-25 19:15:28 +0300

    modify: import other illustrations

--- lib/consts/illustrations.dart
+++ lib/consts/illustrations.dart
@@ -1,5 +1,17 @@
 import 'package:flutter_svg/flutter_svg.dart';
 
-final SvgPicture calendarIllustration = SvgPicture.asset(
+final calendarIllustration = SvgPicture.asset(
   "assets/illustrations/calendar.svg",
+);
+
+final locationIllustration = SvgPicture.asset(
+  "assets/illustrations/location.svg",
+);
+
+final passwordIllustration = SvgPicture.asset(
+  "assets/illustrations/password.svg",
+);
+
+final personalInfoIllustration = SvgPicture.asset(
+  "assets/illustrations/personal_info.svg",
 );
