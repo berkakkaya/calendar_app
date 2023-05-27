aeb9bb5f5048192c0a4465b96dcbd2a985b4c9ed
Berk Akkaya <berkakkaya000@gmail.com>
2023-05-27 12:38:43 +0300

    implement: welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,9 +2,12 @@ import 'package:calendar_app/consts/colors.dart';
 import 'package:calendar_app/consts/fonts.dart';
 import 'package:calendar_app/consts/illustrations.dart';
 import 'package:calendar_app/consts/strings.dart';
+import 'package:calendar_app/models/login_data.dart';
 import 'package:calendar_app/models/response_status.dart';
 import 'package:calendar_app/screens/home_screen.dart';
+import 'package:calendar_app/screens/register_step1_screen.dart';
 import 'package:calendar_app/utils/api.dart';
+import 'package:calendar_app/widgets/popups.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:animations/animations.dart';
@@ -59,13 +62,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               ),
               const Spacer(flex: 1),
               TextField(
-                decoration: InputDecoration(
-                  label: const Text("E-postanız"),
-                  filled: true,
-                  prefixIcon: const Icon(Icons.alternate_email_rounded),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
+                decoration: const InputDecoration(
+                  label: Text("E-postanız"),
+                  prefixIcon: Icon(Icons.alternate_email_rounded),
                   fillColor: color1,
                 ),
                 keyboardType: TextInputType.emailAddress,
@@ -111,46 +110,28 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     password = text;
   }
 
-  Future<void> showInvalidEmailOrPasswordPopup() async {
-    return await showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Oturum açılamadı"),
-          content: const SingleChildScrollView(
-            child: ListBody(
-              children: [
-                Text(wrongEmailOrPassword),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              child: const Text("Tamam"),
-              onPressed: () => Navigator.of(context).pop(),
-            )
-          ],
-        );
-      },
-    );
-  }
-
   Future<void> login(BuildContext context) async {
+    final loginData = LoginData(email: email, password: password);
+
     setState(() {
       loggingIn = true;
     });
 
-    final response = await ApiManager.login(
-      email: email,
-      password: password,
-    );
+    final response = await ApiManager.login(loginData: loginData);
 
     if (response.responseStatus == ResponseStatus.wrongEmailOrPassword) {
-      await showInvalidEmailOrPasswordPopup();
+      if (context.mounted) {
+        await showWarningPopup(
+            context: context,
+            title: "Sunucu hatası",
+            content: [const Text("data")]);
 
-      return setState(() {
-        loggingIn = false;
-      });
+        setState(() {
+          loggingIn = false;
+        });
+      }
+
+      return;
     }
 
     SharedPreferences preferences = await SharedPreferences.getInstance();
@@ -181,6 +162,21 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 
   void goToRegisterScreen(BuildContext context) {
-    print("Triggered.");
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return const RegisterStep1Screen();
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SharedAxisTransition(
+            transitionType: SharedAxisTransitionType.horizontal,
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            fillColor: color1,
+            child: child,
+          );
+        },
+      ),
+    );
   }
 }
