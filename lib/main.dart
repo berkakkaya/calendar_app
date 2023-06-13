16c2653a25b5e0acde5e4b24fcd66cd249b8447b
Berk Akkaya <berkakkaya000@gmail.com>
2023-06-13 23:05:01 +0300

    fix: turkish localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_app/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'consts/colors.dart';
@@ -16,6 +17,13 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Calendar App",
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("tr")],
+      locale: const Locale("tr"),
       theme: ThemeData(
         colorSchemeSeed: color3,
         scaffoldBackgroundColor: color1,
