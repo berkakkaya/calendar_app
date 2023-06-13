af8cdf9b464f2a94007d7318c2beaf9ea4f85840
Berk Akkaya <berkakkaya000@gmail.com>
2023-06-14 01:32:41 +0300

    add: `onTap` to participants card

--- lib/widgets/participants_card.dart
+++ lib/widgets/participants_card.dart
@@ -3,55 +3,60 @@ import 'package:flutter/material.dart';
 
 class ParticipantsCard extends StatelessWidget {
   final int participantCount;
+  final void Function()? onTap;
 
   const ParticipantsCard({
     super.key,
     required this.participantCount,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     assert(participantCount >= 0, "Participant count cannot be lower than 0.");
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: color2,
-      ),
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Icon(Icons.groups_outlined, size: 24, color: color1),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Katılımcıları Düzenle",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleMedium
-                      ?.copyWith(color: color1),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  participantCount == 0
-                      ? "Hiç katılımcı seçilmedi."
-                      : "$participantCount katılımcı seçildi.",
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelMedium
-                      ?.copyWith(color: color1),
-                ),
-              ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: color2,
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(Icons.groups_outlined, size: 24, color: color1),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Katılımcıları Düzenle",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: color1),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    participantCount == 0
+                        ? "Hiç katılımcı seçilmedi."
+                        : "$participantCount katılımcı seçildi.",
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelMedium
+                        ?.copyWith(color: color1),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(width: 16),
-          const Icon(Icons.arrow_forward_rounded, size: 24, color: color1),
-        ],
+            const SizedBox(width: 16),
+            const Icon(Icons.arrow_forward_rounded, size: 24, color: color1),
+          ],
+        ),
       ),
     );
   }
