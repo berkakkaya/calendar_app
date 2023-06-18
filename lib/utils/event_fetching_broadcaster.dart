fd47c0672c534399e4d0ca57730ac2d03c246dfb
Berk Akkaya <berkakkaya000@gmail.com>
2023-06-19 00:23:24 +0300

    implement: event fetching broadcaster

---
+++ lib/utils/event_fetching_broadcaster.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+/// Brodcaster that alerts if fetching the events are needed to its listeners.
+///
+/// This class is a singleton. You can access its instance via
+/// [EventFetchingBroadcaster.i]. The stream can be listened from
+/// `stream` attribute.
+///
+class EventFetchingBroadcaster {
+  static EventFetchingBroadcaster? _i;
+
+  /// Instance of [EventFetchingBroadcaster].
+  static EventFetchingBroadcaster get i {
+    if (_i != null) return _i!;
+
+    _i = EventFetchingBroadcaster._();
+    return _i!;
+  }
+
+  late Stream<void> _stream;
+  Stream<void> get stream => _stream;
+
+  late List<MultiStreamController<void>> _listeners;
+
+  EventFetchingBroadcaster._() {
+    _listeners = [];
+
+    _stream = Stream<void>.multi((listener) {
+      _listeners.add(listener);
+    });
+  }
+
+  /// Broadcast all of its listeners a fetch request.
+  void triggerFetch() {
+    final List<MultiStreamController<void>> markedForRemoval = [];
+
+    for (final listener in _listeners) {
+      if (!listener.isClosed) {
+        listener.add(null);
+      } else {
+        markedForRemoval.add(listener);
+      }
+    }
+
+    // Remove the listeners that have marked for removal
+    for (final listener in markedForRemoval) {
+      _listeners.remove(listener);
+    }
+  }
+}
