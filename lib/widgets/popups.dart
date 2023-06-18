e3d5177ca258c4820c21f175274d175a229d5352
Berk Akkaya <berkakkaya000@gmail.com>
2023-06-18 07:53:01 +0300

    implement: confirmation dialog

--- lib/widgets/popups.dart
+++ lib/widgets/popups.dart
@@ -28,3 +28,45 @@ Future<void> showWarningPopup({
     },
   );
 }
+
+Future<bool> showConfirmationDialog({
+  required BuildContext context,
+  required String title,
+  required List<Widget> content,
+}) async {
+  bool response = false;
+
+  final actions = [
+    TextButton(
+      child: const Text("Hayır"),
+      onPressed: () {
+        response = false;
+        Navigator.of(context).pop();
+      },
+    ),
+    TextButton(
+      child: const Text("Evet"),
+      onPressed: () {
+        response = true;
+        Navigator.of(context).pop();
+      },
+    ),
+  ];
+
+  await showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: content,
+          ),
+        ),
+        actions: actions,
+      );
+    },
+  );
+
+  return response;
+}
